2a751bb3ad046e8fcde8a22c8c3fee385122e651
rusvlp <qdrwlad@gmail.com>
2022-10-20 16:22:33 +0300

    initial commit

---
+++ src/main/java/com/example/swaggertest_init/SwaggerTestInitApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggertest_init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerTestInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerTestInitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swaggertest_init/SwaggerTestInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggertest_init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerTestInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
