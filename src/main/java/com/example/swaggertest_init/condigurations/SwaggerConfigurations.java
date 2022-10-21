672a79c8f174038a08bed60a6b34f2371d5a12c7
rusvlp <qdrwlad@gmail.com>
2022-10-21 09:36:10 +0300

    started configuring Swagger

---
+++ src/main/java/com/example/swaggertest_init/condigurations/SwaggerConfigurations.java
@@ -0,0 +1,10 @@
+package com.example.swaggertest_init.condigurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SwaggerConfigurations {
+
+}
